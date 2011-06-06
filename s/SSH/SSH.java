eb5d67ba9a5ed5482efcb1af067f6b8790da9c31
hschimpf512 <hschimpf512@ae89aa18-ffb6-11de-82af-0555a752a183>
2011-06-06 20:34:35 +0000

    Documentacion de la clase SSH

--- s/SSH/SSH.java
+++ s/SSH/SSH.java
@@ -8,16 +8,16 @@
 package s.SSH;
 
 import u.UserData.UserData;
-import com.jcraft.jsch.Channel;
-import com.jcraft.jsch.JSch;
-import com.jcraft.jsch.JSchException;
-import com.jcraft.jsch.Session;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PipedInputStream;
 import java.io.PipedOutputStream;
 import java.util.regex.Pattern;
+import com.jcraft.jsch.Channel;
+import com.jcraft.jsch.JSch;
+import com.jcraft.jsch.JSchException;
+import com.jcraft.jsch.Session;
 
 /**
  * Clase para conexiones por medio de SSH
@@ -27,51 +27,94 @@ import java.util.regex.Pattern;
  * @version Jun 6, 2011 11:53:26 AM
  */
 public final class SSH {
-	Pattern								alphaNumeric	= Pattern.compile("([^a-zA-z0-9])");
+	private final Pattern			alphaNumeric	= Pattern.compile("([^a-zA-z0-9])");
 
-	Channel								channel;
+	private Channel					channel;
 
-	PipedInputStream					fromServer;
+	private PipedInputStream		fromServer;
 
-	String								lastCommand		= "";
+	private String						lastCommand		= "";
 
-	Session								session;
+	private Session					session;
 
-	PipedOutputStream					toServer;
+	private PipedOutputStream		toServer;
 
 	private static final String	TERMINATOR		= "zDonez";
 
+	/**
+	 * Conecta con el servidor
+	 * 
+	 * @author Hermann D. Schimpf
+	 * @author SCHIMPF - Sistemas de Informacion y Gestion
+	 * @version Jun 6, 2011 4:32:43 PM
+	 * @param username Nombre de usuario
+	 * @param password Contraseña
+	 * @param host Servidor
+	 * @param port Puerto de conexion
+	 * @throws JSchException Error si ya esta actualmente conectado
+	 * @throws IOException Error de escritura o lectura
+	 * @throws InterruptedException Error si se corta la conexion
+	 */
 	public void connect(final String username, final String password, final String host, final int port) throws JSchException, IOException, InterruptedException {
 		final JSch shell = new JSch();
-		this.session = shell.getSession(username, host, port);
+		this.setSession(shell.getSession(username, host, port));
 		final UserData ui = new UserData(username);
 		ui.setPassword(password);
-		this.session.setUserInfo(ui);
-		this.session.connect();
-		this.channel = this.session.openChannel("shell");
+		this.getSession().setUserInfo(ui);
+		this.getSession().connect();
+		this.setChannel(this.getSession().openChannel("shell"));
 		final PipedOutputStream po = new PipedOutputStream();
-		this.fromServer = new PipedInputStream(po);
-		this.channel.setOutputStream(po);
-		this.toServer = new PipedOutputStream();
-		final PipedInputStream pi = new PipedInputStream(this.toServer);
-		this.channel.setInputStream(pi);
-		this.channel.connect();
+		this.setFromServer(new PipedInputStream(po));
+		this.getChannel().setOutputStream(po);
+		this.setToServer(new PipedOutputStream());
+		final PipedInputStream pi = new PipedInputStream(this.getToServer());
+		this.getChannel().setInputStream(pi);
+		this.getChannel().connect();
 		Thread.sleep(100);
 		this.getServerResponse();
 	}
 
+	/**
+	 * Desconecta la instancia del servidor
+	 * 
+	 * @author Hermann D. Schimpf
+	 * @author SCHIMPF - Sistemas de Informacion y Gestion
+	 * @version Jun 6, 2011 4:32:33 PM
+	 */
 	public void disconnect() {
 		if (this.isConnected()) {
-			this.channel.disconnect();
-			this.session.disconnect();
+			this.getChannel().disconnect();
+			this.getSession().disconnect();
 		}
 	}
 
+	/**
+	 * Retorna el ultimo comando ejecutado
+	 * 
+	 * @author Hermann D. Schimpf
+	 * @author SCHIMPF - Sistemas de Informacion y Gestion
+	 * @version Jun 6, 2011 4:23:42 PM
+	 * @return The lastCommand
+	 */
+	public String getLastCommand() {
+		// return the value of lastCommand
+		return this.lastCommand;
+	}
+
+	/**
+	 * Retorna la respuesta recibida del servidor
+	 * 
+	 * @author Hermann D. Schimpf
+	 * @author SCHIMPF - Sistemas de Informacion y Gestion
+	 * @version Jun 6, 2011 4:31:45 PM
+	 * @return Respuesta del servidor
+	 * @throws IOException Error de lectura
+	 * @throws InterruptedException Error si se corta la conexion
+	 */
 	public String getServerResponse() throws IOException, InterruptedException {
 		final StringBuffer builder = new StringBuffer();
-		final int count = 0;
 		String line = "";
-		final BufferedReader reader = new BufferedReader(new InputStreamReader(this.fromServer));
+		final BufferedReader reader = new BufferedReader(new InputStreamReader(this.getFromServer()));
 		while (true) {
 			line = reader.readLine();
 			builder.append(line).append("\n");
@@ -84,19 +127,167 @@ public final class SSH {
 		return result.replaceAll(this.escape(SSH.TERMINATOR), "").trim();
 	}
 
+	/**
+	 * Retorna el estado de la conexion con el servidor
+	 * 
+	 * @author Hermann D. Schimpf
+	 * @author SCHIMPF - Sistemas de Informacion y Gestion
+	 * @version Jun 6, 2011 4:30:04 PM
+	 * @return True si esta conectado
+	 */
 	public boolean isConnected() {
-		// TODO Auto-generated method stub
-		return this.channel != null && this.channel.isConnected();
+		// retornamos si existe un canal y esta conectado
+		return this.getChannel() != null && this.getChannel().isConnected();
 	}
 
-	public void send(String command) throws IOException, InterruptedException {
-		command += "; echo \"" + SSH.TERMINATOR + "\" \n";
-		this.toServer.write(command.getBytes());
+	/**
+	 * Envia un comando al servidor
+	 * 
+	 * @author Hermann D. Schimpf
+	 * @author SCHIMPF - Sistemas de Informacion y Gestion
+	 * @version Jun 6, 2011 4:30:20 PM
+	 * @param command Comando a ejecutar
+	 * @throws IOException Error de escritura
+	 * @throws InterruptedException Error si se corta la conexion
+	 */
+	public void send(final String command) throws IOException, InterruptedException {
+		final String sendCommand = command + "; echo \"" + SSH.TERMINATOR + "\" \n";
+		this.getToServer().write(sendCommand.getBytes());
 		Thread.sleep(100);
-		this.lastCommand = new String(command);
+		this.setLastCommand(new String(sendCommand));
 	}
 
 	private String escape(final String subjectString) {
-		return this.alphaNumeric.matcher(subjectString).replaceAll("\\\\$1");
+		return this.getAlphaNumeric().matcher(subjectString).replaceAll("\\\\$1");
+	}
+
+	/**
+	 * Retorna la cadena de caracteres alfanumericos
+	 * 
+	 * @author Hermann D. Schimpf
+	 * @author SCHIMPF - Sistemas de Informacion y Gestion
+	 * @version Jun 6, 2011 4:24:01 PM
+	 * @return The alphaNumeric
+	 */
+	private Pattern getAlphaNumeric() {
+		// return the value of alphaNumeric
+		return this.alphaNumeric;
+	}
+
+	/**
+	 * Retorna el canal de conexion al servidor
+	 * 
+	 * @author Hermann D. Schimpf
+	 * @author SCHIMPF - Sistemas de Informacion y Gestion
+	 * @version Jun 6, 2011 4:23:33 PM
+	 * @return The channel
+	 */
+	private Channel getChannel() {
+		// return the value of channel
+		return this.channel;
+	}
+
+	/**
+	 * Retorna el InputStream del servidor
+	 * 
+	 * @author Hermann D. Schimpf
+	 * @author SCHIMPF - Sistemas de Informacion y Gestion
+	 * @version Jun 6, 2011 4:23:38 PM
+	 * @return The fromServer
+	 */
+	private PipedInputStream getFromServer() {
+		// return the value of fromServer
+		return this.fromServer;
+	}
+
+	/**
+	 * Retorna la sesion actual
+	 * 
+	 * @author Hermann D. Schimpf
+	 * @author SCHIMPF - Sistemas de Informacion y Gestion
+	 * @version Jun 6, 2011 4:23:47 PM
+	 * @return The session
+	 */
+	private Session getSession() {
+		// return the value of session
+		return this.session;
+	}
+
+	/**
+	 * Retorna el OutputStream del servidor
+	 * 
+	 * @author Hermann D. Schimpf
+	 * @author SCHIMPF - Sistemas de Informacion y Gestion
+	 * @version Jun 6, 2011 4:23:52 PM
+	 * @return The toServer
+	 */
+	private PipedOutputStream getToServer() {
+		// return the value of toServer
+		return this.toServer;
+	}
+
+	/**
+	 * Almacena el canal de conexion
+	 * 
+	 * @author Hermann D. Schimpf
+	 * @author SCHIMPF - Sistemas de Informacion y Gestion
+	 * @version Jun 6, 2011 4:23:33 PM
+	 * @param channel the channel to set
+	 */
+	private void setChannel(final Channel channel) {
+		// set the value of this.channel
+		this.channel = channel;
+	}
+
+	/**
+	 * Almacena el InputStream para el servidor
+	 * 
+	 * @author Hermann D. Schimpf
+	 * @author SCHIMPF - Sistemas de Informacion y Gestion
+	 * @version Jun 6, 2011 4:23:38 PM
+	 * @param fromServer the fromServer to set
+	 */
+	private void setFromServer(final PipedInputStream fromServer) {
+		// set the value of this.fromServer
+		this.fromServer = fromServer;
+	}
+
+	/**
+	 * Almacena el ultimo comando ejecutado
+	 * 
+	 * @author Hermann D. Schimpf
+	 * @author SCHIMPF - Sistemas de Informacion y Gestion
+	 * @version Jun 6, 2011 4:23:42 PM
+	 * @param lastCommand the lastCommand to set
+	 */
+	private void setLastCommand(final String lastCommand) {
+		// set the value of this.lastCommand
+		this.lastCommand = lastCommand;
+	}
+
+	/**
+	 * Almacena la sesion para utilizar en el server
+	 * 
+	 * @author Hermann D. Schimpf
+	 * @author SCHIMPF - Sistemas de Informacion y Gestion
+	 * @version Jun 6, 2011 4:23:47 PM
+	 * @param session the session to set
+	 */
+	private void setSession(final Session session) {
+		// set the value of this.session
+		this.session = session;
+	}
+
+	/**
+	 * Almacena el OutputStream del servidor
+	 * 
+	 * @author Hermann D. Schimpf
+	 * @author SCHIMPF - Sistemas de Informacion y Gestion
+	 * @version Jun 6, 2011 4:23:52 PM
+	 * @param toServer the toServer to set
+	 */
+	private void setToServer(final PipedOutputStream toServer) {
+		// set the value of this.toServer
+		this.toServer = toServer;
 	}
 }
