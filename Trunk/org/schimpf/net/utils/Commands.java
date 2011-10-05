6004392cd267db40eae89da0802c33bae7eda6d6
hschimpf512 <hschimpf512@ae89aa18-ffb6-11de-82af-0555a752a183>
2011-10-05 04:01:03 +0000

    Agregados mas comandos de comunicacion para inicializacion del socket

--- Trunk/org/schimpf/net/utils/Commands.java
+++ Trunk/org/schimpf/net/utils/Commands.java
@@ -14,35 +14,66 @@ package org.schimpf.net.utils;
  * @author Schimpf.NET
  * @version Aug 5, 2011 10:16:24 AM
  */
-public final class Commands {
+public enum Commands {
+	/**
+	 * Comando de despedida
+	 * 
+	 * @version Oct 4, 2011 11:49:02 PM
+	 */
+	BYE,
+	/**
+	 * Comando para empezar el envio de datos
+	 * 
+	 * @version Oct 5, 2011 12:19:13 AM
+	 */
+	DATA,
 	/**
 	 * Comando de salir
 	 * 
 	 * @version Aug 5, 2011 10:08:00 AM
 	 */
-	public static final String	EXIT		= "exit";
-
+	EXIT,
 	/**
 	 * Comando de saludo
 	 * 
 	 * @version Aug 5, 2011 10:20:41 AM
 	 */
-	public static final String	HELLO		= "hello";
-
+	HELO,
+	/**
+	 * Comando de datos recibidos
+	 * 
+	 * @version Oct 4, 2011 11:42:54 PM
+	 */
+	OK,
 	/**
 	 * Comando para finalizar el programa
 	 * 
 	 * @version Aug 5, 2011 3:58:20 PM
 	 */
-	public static final String	SHUTDOWN	= "shutdown";
-
+	SHUTDOWN,
 	/**
-	 * Eliminamos el constructor
+	 * Comando para identificar el inicio de datos
 	 * 
-	 * @author Hermann D. Schimpf
-	 * @author SCHIMPF - Sistemas de Informacion y Gestion
-	 * @author Schimpf.NET
-	 * @version Aug 5, 2011 10:32:15 AM
+	 * @version Oct 5, 2011 12:28:14 AM
 	 */
-	private Commands() {}
+	START;
+	/**
+	 * Retorna el comando desde su nombre
+	 * 
+	 * @author <FONT style='color:#55A; font-size:12px; font-weight:bold;'>Hermann D. Schimpf</FONT>
+	 * @author <B>SCHIMPF</B> - <FONT style="font-style:italic;">Sistemas de Informaci&oacute;n y Gesti&oacute;n</FONT>
+	 * @author <B>Schimpf.NET</B>
+	 * @version Oct 5, 2011 12:06:24 AM
+	 * @param command Nombre del comando
+	 * @return Comando
+	 */
+	public static Commands get(final String command) {
+		try {
+			// retornamos el comando
+			return Commands.valueOf(command);
+		} catch (final IllegalArgumentException e) {
+			// retornamos null
+			return null;
+		}
+	}
 }
