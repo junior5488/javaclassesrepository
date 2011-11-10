56efdac192d6e741dbf56b18339303e439dfadf1
hschimpf512 <hschimpf512@ae89aa18-ffb6-11de-82af-0555a752a183>
2011-11-10 14:09:41 +0000

    Comments

--- Trunk/sql/src/org/schimpf/sql/mysql/MySQLProcess.java
+++ Trunk/sql/src/org/schimpf/sql/mysql/MySQLProcess.java
@@ -1,6 +1,18 @@
 /**
- * @author Hermann D. Schimpf
- * @author SCHIMPF - Sistemas de Informacion y Gestion
+ * | This program is free software: you can redistribute it and/or modify
+ * | it under the terms of the GNU General Public License as published by
+ * | the Free Software Foundation, either version 3 of the License.
+ * |
+ * | This program is distributed in the hope that it will be useful,
+ * | but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * | GNU General Public License for more details.
+ * |
+ * | You should have received a copy of the GNU General Public License
+ * | along with this program. If not, see <http://www.gnu.org/licenses/>.
+ * 
+ * @author <FONT style='color:#55A; font-size:12px; font-weight:bold;'>Hermann D. Schimpf</FONT>
+ * @author <B>SCHIMPF</B> - <FONT style="font-style:italic;">Sistemas de Informaci&oacute;n y Gesti&oacute;n</FONT>
  * @version Apr 16, 2011 2:00:45 AM
  */
 package org.schimpf.sql.mysql;
@@ -10,14 +22,14 @@ import org.schimpf.sql.base.SQLProcess;
 /**
  * Clase para ejecutar consultas SQL en servidores de Bases de Datos MySQL
  * 
- * @author Hermann D. Schimpf
- * @author SCHIMPF - Sistemas de Informacion y Gestion
+ * @author <FONT style='color:#55A; font-size:12px; font-weight:bold;'>Hermann D. Schimpf</FONT>
+ * @author <B>SCHIMPF</B> - <FONT style="font-style:italic;">Sistemas de Informaci&oacute;n y Gesti&oacute;n</FONT>
  * @version Apr 16, 2011 2:00:45 AM
  */
 public final class MySQLProcess extends SQLProcess {
 	/**
-	 * @author Hermann D. Schimpf
-	 * @author SCHIMPF - Sistemas de Informacion y Gestion
+	 * @author <FONT style='color:#55A; font-size:12px; font-weight:bold;'>Hermann D. Schimpf</FONT>
+	 * @author <B>SCHIMPF</B> - <FONT style="font-style:italic;">Sistemas de Informaci&oacute;n y Gesti&oacute;n</FONT>
 	 * @version Apr 16, 2011 2:00:45 AM
 	 */
 	public MySQLProcess() {
