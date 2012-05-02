bb1db0c7c5c69b71bf8571c03286b5e513fbeeed
hschimpf512 <hschimpf512@ae89aa18-ffb6-11de-82af-0555a752a183>
2012-05-02 03:38:45 +0000

    Cargamos los metadatos de la columna

--- Trunk/sql/src/org/schimpf/sql/pgsql/wrapper/PGColumn.java
+++ Trunk/sql/src/org/schimpf/sql/pgsql/wrapper/PGColumn.java
@@ -20,6 +20,8 @@ package org.schimpf.sql.pgsql.wrapper;
 
 import org.schimpf.sql.base.wrappers.ColumnWrapper;
 import org.schimpf.sql.pgsql.PostgreSQLProcess;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 /**
  * Datos de la columna PostgreSQL
@@ -41,5 +43,51 @@ public final class PGColumn extends ColumnWrapper<PostgreSQLProcess> {
 	public PGColumn(final PostgreSQLProcess sqlConnector, final String columnName) {
 		// enviamos el constructor
 		super(sqlConnector, columnName);
+	}
+
+	@Override
+	public String toString() {
+		try {
+			// retornamos la definicion de la columna
+			return this.getColumnName() + " " + this.getDataType() + (this.getLength() != null ? "(" + this.getLength() + (this.getPrecision() != null ? ", " + this.getPrecision() : "") + ")" : "") + (this.isNull() ? "" : "NOT ") + "NULL" + (this.getDefaultValue() != null ? "DEFAULT" + this.getDefaultValue() : "");
+		} catch (SQLException e) {}
+		// retornamos el nombre de la columna
+		return this.getColumnName();
+	}
+
+	@Override
+	protected String getDataTypeFromMetadata(final ResultSet metadata) throws SQLException {
+		// retonamos el tipo de dato
+		return metadata.getString("data_type");
+	}
+
+	@Override
+	protected String getDefaultValueFromMetadata(final ResultSet metadata) throws SQLException {
+		// retornamos el valor por defecto
+		return metadata.getString("default_value");
+	}
+
+	@Override
+	protected Boolean getIsNullFromMetadata(final ResultSet metadata) throws SQLException {
+		// retornamos si permite valores nulos
+		return metadata.getString("is_nullable").equals("YES") ? true : false;
+	}
+
+	@Override
+	protected Integer getLengthFromMetadata(final ResultSet metadata) throws SQLException {
+		// retornamos el tamaño del campo
+		return this.getDataType().endsWith("char") ? metadata.getInt("srt_length") : metadata.getInt("num_length");
+	}
+
+	@Override
+	protected Integer getPrecisionFromMetadata(final ResultSet metadata) throws SQLException {
+		// retornamos la presicion de la columna
+		return metadata.getInt("precision_dec");
+	}
+
+	@Override
+	protected boolean retrieveColumnMetadata(final String columnName) {
+		// retornamos el resultado del SQL
+		return this.getSQLConnector().executeSQL("SELECT column_name, udt_name AS data_type, character_maximum_length AS str_length, numeric_precision AS num_length, numeric_precision_radix AS precision_dec, is_nullable, column_default AS default_value FROM information_schema.columns WHERE table_name ILIKE '" + columnName + "'");
 	}
 }
