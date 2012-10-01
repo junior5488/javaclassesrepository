b23b46588a1edc353c467384cc2ee4fbaad9030d
hschimpf512 <hschimpf512@ae89aa18-ffb6-11de-82af-0555a752a183>
2012-10-01 21:04:52 +0000

    SortableList: Utilizamos la clase Collections para ordenar

--- Trunk/util/src/org/schimpf/util/arrays/SortableList.java
+++ Trunk/util/src/org/schimpf/util/arrays/SortableList.java
@@ -6,6 +6,7 @@
 package org.schimpf.util.arrays;
 
 import java.util.ArrayList;
+import java.util.Collections;
 
 /**
  * Lista de objetos ordenable por medio de la comparacion de los objetos mediante el metodo compareTo() de los objetos
@@ -22,24 +23,6 @@ public final class SortableList<Type extends Comparable<? super Type>> extends A
 	 * @version Sep 13, 2011 10:27:44 PM
 	 */
 	private static final long	serialVersionUID	= 1L;
-
-	/**
-	 * Lista de elementos a ordenar
-	 * 
-	 * @author Hermann D. Schimpf
-	 * @author SCHIMPF - Sistemas de Informacion y Gestion
-	 * @version Feb 10, 2011 2:44:16 PM
-	 */
-	private ArrayList<Type>		listToSort;
-
-	/**
-	 * Lista de elementos ordenados
-	 * 
-	 * @author Hermann D. Schimpf
-	 * @author SCHIMPF - Sistemas de Informacion y Gestion
-	 * @version Feb 10, 2011 2:44:01 PM
-	 */
-	private ArrayList<Type>		sortedList;
 
 	/**
 	 * Ordena la lista de forma ascendente
@@ -62,139 +45,11 @@ public final class SortableList<Type extends Comparable<? super Type>> extends A
 	 * @param asc True para ordenar de forma ascendente
 	 */
 	public void sort(final boolean asc) {
-		// limpiamos y obtenemos los valores a ordenar
-		this.cleanAndGetAll();
 		// ordenamos la lista
-		this.sortList(asc);
-		// regeneramos la lista local
-		this.regenerateList();
-	}
-
-	/**
-	 * Limpia las listas y obtiene los valores actuales a ordenar
-	 * 
-	 * @author Hermann D. Schimpf
-	 * @author SCHIMPF - Sistemas de Informacion y Gestion
-	 * @version Jan 12, 2011 11:56:26 AM
-	 */
-	private void cleanAndGetAll() {
-		// vaciamos las listas
-		this.cleanListToSort();
-		this.cleanSortedList();
-		// recorremos la lista
-		for (int i = 0; i < this.size(); i++)
-			// agregamos el objeto a la lista local
-			this.getListToSort().add(this.get(i));
-	}
-
-	/**
-	 * Limpia la lista a ordenar
-	 * 
-	 * @author Hermann D. Schimpf
-	 * @author SCHIMPF - Sistemas de Informacion y Gestion
-	 * @version Feb 10, 2011 2:46:51 PM
-	 */
-	private void cleanListToSort() {
-		// limpiamos la lista a ordenar
-		this.listToSort = new ArrayList<Type>();
-	}
-
-	/**
-	 * Limpia la lista ordenada
-	 * 
-	 * @author Hermann D. Schimpf
-	 * @author SCHIMPF - Sistemas de Informacion y Gestion
-	 * @version Feb 10, 2011 2:44:59 PM
-	 */
-	private void cleanSortedList() {
-		// set the value of this.sortedList
-		this.sortedList = new ArrayList<Type>();
-	}
-
-	/**
-	 * Retorna la lista a ordenar
-	 * 
-	 * @author Hermann D. Schimpf
-	 * @author SCHIMPF - Sistemas de Informacion y Gestion
-	 * @version Feb 10, 2011 2:46:51 PM
-	 * @return Lista a ordenar
-	 */
-	private ArrayList<Type> getListToSort() {
-		// retornamos la lista a ordenar
-		return this.listToSort;
-	}
-
-	/**
-	 * Retorna la lista ordenada
-	 * 
-	 * @author Hermann D. Schimpf
-	 * @author SCHIMPF - Sistemas de Informacion y Gestion
-	 * @version Feb 10, 2011 2:44:59 PM
-	 * @return Lista ordenada
-	 */
-	private ArrayList<Type> getSortedList() {
-		// return the value of sortedList
-		return this.sortedList;
-	}
-
-	/**
-	 * Reemplaza la lista local con los nuevos elementos ordenados
-	 * 
-	 * @author Hermann D. Schimpf
-	 * @author SCHIMPF - Sistemas de Informacion y Gestion
-	 * @version Feb 10, 2011 2:43:23 PM
-	 */
-	private void regenerateList() {
-		// vaciamos la lista local
-		this.clear();
-		// recorremos la lista ordenada
-		for (final Type element: this.getSortedList())
-			// agregamos el elemento a la lista local
-			this.add(element);
-	}
-
-	/**
-	 * Ordena la lista de elementos
-	 * 
-	 * @author Hermann D. Schimpf
-	 * @author SCHIMPF - Sistemas de Informacion y Gestion
-	 * @version Feb 10, 2011 2:42:11 PM
-	 */
-	private void sortList(final boolean asc) {
-		// diferencia en la comparacion
-		int diff;
-		// indices ya utilizados
-		final ArrayList<Integer> indexes = new ArrayList<Integer>();
-		// recorremos las posiciones nuevas
-		for (int n = 0; n <= this.size(); n++)
-			// recorremos la lista de valores originales
-			for (int i = 0; i <= this.getListToSort().size(); i++) {
-				// verificamos si el indice ya se utilizo
-				if (indexes.contains(i))
-					// saltamos el indice
-					continue;
-				// seteamos la diferencia por defecto
-				diff = 0;
-				// verificamos si existe el nuevo dato
-				if (n < this.getSortedList().size() && i < this.getListToSort().size())
-					// obtenemos la diferencia
-					diff = this.getListToSort().get(i).compareTo(this.getSortedList().get(n));
-				// verificamos si es menor o mayor
-				if (i < this.getListToSort().size() && (asc && diff <= 0 || !asc && diff >= 0)) {
-					// verificamos si tenemos elementos
-					if (n < indexes.size())
-						// agregamos el indice a los utilizados
-						indexes.set(n, i);
-					else
-						indexes.add(i);
-					// verificamos si tenemos elementos
-					if (n < this.getSortedList().size())
-						// reemplazamos el objeto por el nuevo
-						this.getSortedList().set(n, this.getListToSort().get(i));
-					else
-						// agregamos el elemento
-						this.getSortedList().add(this.getListToSort().get(i));
-				}
-			}
+		Collections.sort(this);
+		// verificamos si es al revez
+		if (!asc)
+			// invertimos la lista
+			Collections.reverse(this);
 	}
 }
